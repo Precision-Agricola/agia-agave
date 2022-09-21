61eb2ade010c9e8873803079baa81b77a4d54dff
zaned897 <3dmena@gmail.com>
2022-09-21 07:27:39 -0500

    feat: new utils for image batch preprocessing

--- core/core_utils.py
+++ core/core_utils.py
@@ -2,6 +2,7 @@
 
 from PIL import Image, ImageOps
 from pathlib import Path
+from sympy import factorint
 
 class CoreUtils:
 	"""Core Module Utilities for image processing and other functions.
@@ -56,8 +57,8 @@ class CoreUtils:
 			raise IndexError("Each image in set must be formatted in grayscale")
 		return Image.merge("RGB", (images[0], images[1], images[2]))
 
-	def sample_image(image:Image, size: tuple, slice_no:int) -> Image:
-		"""Get a sample of the image"""
+	def get_segment_by_position(image:Image, size: tuple, slice_no:int) -> Image:
+		"""Get a sample of the image corresponding to n file and m column."""
 		rows = size[0]
 		cols = size[1]
 		if slice_no > rows*cols:
@@ -73,3 +74,22 @@ class CoreUtils:
 		top = row * slice_height
 		bottom = top + slice_height
 		return image.crop((left, top, right, bottom))
+
+	def get_segment_by_index(image:Image, sample_no:int, samples:int) -> Image:
+		"""Get a sample of the image given the sample number and the total number of samples."""
+		if sample_no > samples:
+			raise ValueError("Sample number out of range")
+		factors = factorint(samples)
+		factored = list(factors.items())
+		if len(factored) == 1 and factored[0][1] == 2:
+			rows = factored[0][0]
+			cols = factored[0][0]
+		else:
+			rows = factored[0][0]**factored[0][1]
+			cols = sum([factored[i][0]**factored[i][1] for i in range(1, len(factors))])
+		element_row = sample_no // rows 
+		element_col = sample_no // cols + sample_no % cols - element_row
+		width, heigh = Image.size[0], Image.size[1]
+
+
+		
