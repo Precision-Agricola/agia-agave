0afa255bb70a9b134576471efc614c905d3f41c0
zaned897 <3dmena@gmail.com>
2022-12-01 04:16:31 -0600

    fix: wrogn rows, cols assign

--- core/image_utils.py
+++ core/image_utils.py
@@ -24,12 +24,12 @@ class ImageUtils:
         cols = matrix_size[1]
         if slice_tuple[0]*slice_tuple[1] > rows*cols:
             raise ValueError("Slice number out of range")
-        width = image.shape[0]
-        height = image.shape[1]
+        height = image.shape[0]
+        width = image.shape[1]
         slice_width = width // cols
         slice_height = height // rows
-        row = slice_tuple[0]
-        col = slice_tuple[1]
+        row = slice_tuple[0] - 1
+        col = slice_tuple[1] - 1
         left = col * slice_width
         right = left + slice_width
         top = row * slice_height
@@ -38,18 +38,19 @@ class ImageUtils:
 
     def get_slices_params(image:array, slices_no:int, sample_index:int) -> array:
         """Segment the image according the factors fo the slices number."""
-        if sample_index > slices_no:
+        if sample_index >= slices_no or sample_index < 0:
             raise ValueError("Slice number out of range")
         factors = factorint(slices_no)
         factored = list(factors.items())
+        factored.sort(key=lambda x: x[1], reverse=True)
         if len(factored) == 1 and factored[0][1] == 2:
             rows = factored[0][0]
             cols = factored[0][0]
         else:
             rows = factored[0][0]**factored[0][1]
             cols = sum([factored[i][0]**factored[i][1] for i in range(1, len(factored))])
-        element_row = sample_index // cols + 1
-        element_col = sample_index % cols
+        element_row = (sample_index // cols) + 1
+        element_col = (sample_index % cols) + 1
         if image.shape[0] < image.shape[1]:
             return ((rows, cols), (element_row, element_col))
         return ((cols, rows), (element_row, element_col))
