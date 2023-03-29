0719a9ec3593c5d646317107cbd46bdac1576eaf
zaned897 <3dmena@gmail.com>
2023-03-29 14:42:20 -0600

    PAA-17 feat: torch model export

--- sandbox_retrain.py
+++ sandbox_retrain.py
@@ -1,21 +1,26 @@
-#%% import dense segmentation model (RestNet50 backbone)
+""" This code shows the general steps to retrain a model using the deepforest package.
+    The model can be loaded by creating a deepforest instance and then loading the state_dict."""
+
+import torch
 from deepforest import main, get_data
 from deepforest.main import deepforest
 from wasabi import Printer
+import os
 msg = Printer()
 
-import os
-
 class AgaveModel:
+  """This class is used to retrain the agave model."""
   def __init__(self):
     self.annotations_file = get_data(os.path.join(os.getcwd(),"DATA", "agave", "train.csv"))
     self.validation_file = get_data(os.path.join(os.getcwd(),"DATA", "agave", "test.csv"))
     self.save_dir = os.path.join(os.getcwd(),'pred_result')
 
   def model_init(self):
+    """Initialize the model."""
     return deepforest()
   
   def model_gpu_config(self, model):
+    """Configure the model to use GPU and the hyperparameters."""
     model.config['workers'] = 0 #use all the CPU resources [0]
     model.config['gpus'] = '-1' #move to GPU and use all the GPU resources
     model.config["train"]["csv_file"] = self.annotations_file
@@ -29,15 +34,18 @@ class AgaveModel:
     return model
 
   def model_trainer(self, model):
+    """Create the trainer and use the release model."""
     model.create_trainer()
     model.use_release()
     return model
 
   def model_train(self, model):
+    """Train the model."""
     model.trainer.fit(model)
     return model
   
   def model_save_result(self, model):
+    """Save the model and evaluate the model."""
     if not os.path.exists(self.save_dir):
         os.makedirs(self.save_dir)
     results = model.evaluate(
@@ -46,10 +54,13 @@ class AgaveModel:
     iou_threshold = 0.4,
     savedir= self.save_dir
     )
+    model_name = "harvest_perception_bird_release.pt"
+    model_path = os.path.join(self.save_dir, model_name)
+    torch.save(model.state_dict(), model_path)
     return results
 
-
 def main():
+    """Main function."""
     agave_model = AgaveModel()
     model = agave_model.model_init()
     model = agave_model.model_gpu_config(model)
@@ -58,35 +69,7 @@ def main():
     results = agave_model.model_save_result(model)
     msg.info(f"Model precision: {results['box_precision']}")
     msg.info(f"Model recall: {results['box_recall']}")
+    model.save()
 
-
-#%%
-agave_model = AgaveModel()
-model = agave_model.model_init()
-model = agave_model.model_gpu_config(model)
-model = agave_model.model_trainer(model)
-model = agave_model.model_train(model)
-results = agave_model.model_save_result(model)
-msg.info(f"Model precision: {results['box_precision']}")
-msg.info(f"Model recall: {results['box_recall']}")
-
-
-#%% save the model using pt format
-
-import torch
-from os.path import join
-from deepforest import main as df_main
-from matplotlib import pyplot as plt
-
-model_path = join('temp_folder','bird_release.pt')
-predict_path = join('temp_folder','ortomosaic', 'ortomosaicsegment_0_1.png')
-
-df_main = df_main.deepforest()
-df_main.load_state_dict(state_dict=torch.load(model_path))
-df_main.use_release()
-
-image = df_main.predict_image(path=predict_path, return_plot=True)
-image = image[:,:,::-1]
-
-plt.imshow(image)
-
+if __name__ == "__main__":
+    main()
